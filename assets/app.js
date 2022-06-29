57caf6278bafc7ea9cb09beb33b94d9c25e8634e
Jahid <jahidhasan.developer@gmail.com>
2022-06-30 03:35:47 +0500

    added

---
+++ assets/app.js
@@ -0,0 +1,10 @@
+function leftMenuToggle(){
+    const leftMenu = document.querySelector('#left_col');
+    const left_menu = document.querySelector('.left_menu');
+    const topMenu = document.querySelector('#right_full');
+    const menuIcon = document.querySelector('.fa-bars');
+    menuIcon.classList.toggle('active');
+    topMenu.classList.toggle('col-md-10');
+    leftMenu.classList.toggle('col-md-2')
+    left_menu.classList.toggle('active');
+}
