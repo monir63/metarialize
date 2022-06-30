dd5a4bdafa4c7ac4ef7a69dc57d7dbb157430a9a
Jahid <jahidhasan.developer@gmail.com>
2022-07-01 00:31:53 +0500

    update pegination

--- assets/app.js
+++ assets/app.js
@@ -1,27 +1,43 @@
-function leftMenuToggle(){
+function leftMenuToggle(ind){
     const leftMenu = document.querySelector('#left_col');
     const left_menu = document.querySelector('.left_menu');
     const topMenu = document.querySelector('#right_full');
     const menuIcon = document.querySelector('.fa-bars');
-    menuIcon.classList.toggle('active');
+    if(ind === false){
+        menuIcon.classList.remove('active');
+        topMenu.classList.remove('col-md-10');
+        leftMenu.classList.remove('col-md-2')
+        left_menu.classList.remove('active');
+    }else{
+        menuIcon.classList.toggle('active');
     topMenu.classList.toggle('col-md-10');
     leftMenu.classList.toggle('col-md-2')
     left_menu.classList.toggle('active');
+    }
 }
 
-function toggleRightInput(){
+function toggleRightInput(ind){
     const rightMenu = document.querySelector('.rightSideInput');
     const full_body_from_right = document.querySelector('.full_body_from_right');
-    full_body_from_right.classList.toggle('active');
+    if(ind === false){
+        full_body_from_right.classList.remove('active');
+        rightMenu.classList.remove('active');
+    }else{
+        full_body_from_right.classList.toggle('active');
     rightMenu.classList.toggle('active');
+    }
 }
-function menuItemToggle(item){
+function menuItemToggle(item,ind){
     const menuItem = document.getElementById(item);
-    menuItem.classList.toggle('active');
+    if(ind===false){
+        menuItem.classList.remove('active');
+    }else{
+        menuItem.classList.toggle('active');
+    }
 }
 function removeActiveFunction(){
-    menuItemToggle('fixedProfile')
-    menuItemToggle('notification')
-    toggleRightInput()
-    leftMenuToggle()
+    menuItemToggle('fixedProfile',false)
+    menuItemToggle('notification',false)
+    toggleRightInput(false)
+    leftMenuToggle(false)
 }
